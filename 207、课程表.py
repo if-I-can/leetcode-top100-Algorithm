1776b03a7ef5d476c2c54ebf642cbdf16dc571a3
if-I-can <149884707+if-I-can@users.noreply.github.com>
2025-01-25 11:51:44 +0800

    1.25-1

--- 207、课程表.py
+++ 207、课程表.py
@@ -1,4 +1,10 @@
-from collections import defaultdict
+"""
+两种思路：1 图结构，若存在环，则说明会出现冲突  (暂不考虑)
+2 深度有限搜索，如果整在访问的节点再一次被访问，则会出现冲突。（先看这种方法）
+
+
+这题难度还是很高的。 但我做起来感觉还是挺顺的，把逻辑捋顺
+"""
 
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
@@ -7,11 +13,33 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        graph = defaultdict(list)
-        for k,v in graph:
-            graph(v).append(k)
-        print(graph)
 
+        graph = [[] for _ in range(numCourses)]
+        
+        for k,v in prerequisites:
+            graph[k].append(v)
+
+        visited = [0] * numCourses
+
+        def dfs(course):
+            if visited[course] == 1:
+                return False
+            if visited[course] == 2:
+                return True
+            
+            visited[course] = 1
+            for j in graph[course]:
+                if not dfs(j):
+                    return False
+            visited[course] = 2
+
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+            
 
 
 
